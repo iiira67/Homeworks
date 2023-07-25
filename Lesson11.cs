9322169735b56b230252ab3c84418cce58805932
iiira67 <137424567+iiira67@users.noreply.github.com>
2023-07-25 10:15:16 +0300

    Update Lesson11.cs
    Новое решение задачи36

--- Lesson11.cs
+++ Lesson11.cs
@@ -3,30 +3,18 @@
 //[-4, -6, 89, 6] -> 0
 
 
+
 int[] nums = new int[4];
-FillArrayRandomNumbers(nums);
-PrintArray(nums);
 int sum = 0;
+for (int i = 0; i < nums.Length; i++)
+{
+    nums[i] = new Random().Next(-10, 10);
+    Console.Write(nums[i] + " ");
+}
 
 for (int i = 1; i < nums.Length; i += 2)
 {
-    sum += nums[i];
+    sum = sum + nums[i];
 }
-Console.Write(sum);
-
-void FillArrayRandomNumbers(int[] array)
-{
-    for (int i = 0; i < array.Length; i++)
-    {
-        array[i] = new Random().Next(-100, 101);
-    }
-}
-
-void PrintArray(int[] array)
-{
-    for (int i = 0; i < array.Length; i++)
-    {
-        Console.Write(array[i] + " ");
-    }
-    Console.WriteLine();
-}
+Console.WriteLine();
+Console.Write($"Cумма элементов, стоящих на нечётных позициях: {sum}");
