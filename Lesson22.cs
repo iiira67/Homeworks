12cb2d18b335a134e08e244e37ec90d4cecdfb32
iiira67 <137424567+iiira67@users.noreply.github.com>
2023-08-04 10:25:05 +0300

    Update Lesson22.cs
    Корректировка

--- Lesson22.cs
+++ Lesson22.cs
@@ -4,42 +4,39 @@
 // 11 16 15 6
 // 10 9 8 7
 
-Console.Clear();
-Console.WriteLine($"Задача 62: Заполните спирально массив 4 на 4.");
 
-int n = 4;
-int[,] sqareMatrix = new int[n, n];
+int[,] Matrix = new int[4, 4];
 
 int temp = 1;
 int i = 0;
 int j = 0;
 
-while (temp <= sqareMatrix.GetLength(0) * sqareMatrix.GetLength(1))
+while (temp <= Matrix.GetLength(0) * Matrix.GetLength(1))
 {
-  sqareMatrix[i, j] = temp;
+  Matrix[i, j] = temp;
   temp++;
-  if (i <= j + 1 && i + j < sqareMatrix.GetLength(1) - 1)
+  if (i <= j + 1 && i + j < Matrix.GetLength(1) - 1)
     j++;
-  else if (i < j && i + j >= sqareMatrix.GetLength(0) - 1)
+  else if (i < j && i + j >= Matrix.GetLength(0) - 1)
     i++;
-  else if (i >= j && i + j > sqareMatrix.GetLength(1) - 1)
+  else if (i >= j && i + j > Matrix.GetLength(1) - 1)
     j--;
   else
     i--;
 }
 
-WriteArray(sqareMatrix);
+PrintArray(Matrix);
 
-void WriteArray (int[,] array)
+void PrintArray(int[,] matrix)
 {
-  for (int i = 0; i < array.GetLength(0); i++)
+  for (int i = 0; i < matrix.GetLength(0); i++)
   {
-    for (int j = 0; j < array.GetLength(1); j++)
+    for (int j = 0; j < matrix.GetLength(1); j++)
     {
-      if (array[i,j] / 10 <= 0)
-      Console.Write($" {array[i,j]} ");
+      if (matrix[i,j] / 10 <= 0)
+      Console.Write($" {matrix[i,j]} ");
 
-      else Console.Write($"{array[i,j]} ");
+      else Console.Write($"{matrix[i,j]} ");
     }
     Console.WriteLine();
   }
